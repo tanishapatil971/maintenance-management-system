60f7eb391888f59d33b7b7bbd80363ce6880fc7b
Tanisha Patil <tanishapatil971@gmail.com>
2026-07-24 18:59:57 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/maintenance-management-system/',
   plugins: [react()],
+  base: '/maintenance-management-system/',
 })
