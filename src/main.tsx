58e1ddcfdb925ca4c13075ab318033946d65d1b1
Tanisha Rajendra Patil <tanupatil971@gmail.com>
2026-07-24 20:42:28 +0530

    Remove HashRouter and update App import

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'antd/dist/reset.css';
-import { HashRouter } from 'react-router-dom';
 
-import App from './App.tsx';
+import App from './App';
 import { seedDemoData } from './utils/demoSeed';
 
 seedDemoData();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 );
